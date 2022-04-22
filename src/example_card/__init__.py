6fba52d7aef78422bdf3eb695c7d2784887f449e
Daniel Y <33942379+qanelph@users.noreply.github.com>
2022-04-22 14:04:44 +0300

    Initialize singlepage

---
+++ src/example_card/__init__.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from jinja2 import Environment
+
+from supervisely.app import StateJson, DataJson
+
+import src.sly_globals as g
+
+from src.example_card.card_handlers import *
+from src.example_card.card_functions import *
+from src.example_card.card_widgets import *
+
+
+##############
+# init fields
+##############
+
+StateJson()['showDebugViewer'] = True
+
+
+##############
+# init routes
+##############
+
+g.app.add_api_route('/some_post_request_from_frontend/', card_handlers.example_route, methods=["POST"])
+
+
