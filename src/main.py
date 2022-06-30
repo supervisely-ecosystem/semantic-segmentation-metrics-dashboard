981431ed5e5690b80fa6979b4851e45ed567385e
Daniel Y <33942379+qanelph@users.noreply.github.com>
2022-06-30 16:14:16 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import Request, Depends
 from supervisely.app import StateJson
+from supervisely.app.fastapi import available_after_shutdown
 
 import src.sly_globals as g
 
@@ -9,14 +10,21 @@ import src.select_input_classes
 import src.segmentation_metrics_dashboard
 
 
+
 # @TODO: hide confusion matrix totals (row / col)
 # @TODO: confusion visualize all TP/FN (?) by specific class
 # @TODO: ? fixed colors to blue mask
 
 
 @g.app.get("/")
-def read_index(request: Request):
+@available_after_shutdown(app=g.app)
+def read_index(request: Request = None):
     return g.templates_env.TemplateResponse('index.html', {'request': request})
+
+
+@g.app.on_event("shutdown")
+def shutdown():
+    read_index()  # save last version of static files
 
 
 @g.app.post("/apply_changes/")
